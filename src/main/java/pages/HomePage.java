699cef8580704f22f5965a758e24b04acf43e3ba
naresh-kandula <kandulanaresh06@gmail.com>
2026-04-24 23:15:42 +0530

    Added Sign-in popup handle

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -4,6 +4,8 @@ import drivers.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import static utils.WaitUtils.waitForClickable;
+
 public class HomePage {
 
     WebDriver driver;
@@ -14,11 +16,15 @@ public class HomePage {
 
     // locator
     private By body = By.xpath("//body");
+    private By close_signUp_dialog = By.xpath("//*[@class='commonModal__close']");
 
     public void closePopup() {
         try {
             Thread.sleep(3000);
             driver.findElement(body).click();
+            waitForClickable(close_signUp_dialog);
+            driver.findElement(close_signUp_dialog).click();
+            Thread.sleep(10000);
         } catch (Exception e) {
             System.out.println("Popup not displayed");
         }
